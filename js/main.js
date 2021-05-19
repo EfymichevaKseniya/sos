c00e670953da5e62cda5b67416b9c93eb73e897e
Ефимычева Ксения <cocraren359@gmail.com>
2021-05-19 13:42:21 +0400

    добавила плавный скролл

--- js/main.js
+++ js/main.js
@@ -146,8 +146,24 @@ function slider() {
             });
 }
 
+function smoothScroll() {
+	const scrollLinks = document.querySelectorAll('a.scroll-link');
+
+	for (const scrollLink of scrollLinks) {
+		scrollLink.addEventListener('click', event => {
+			event.preventDefault();
+			const id = scrollLink.getAttribute('href');
+			document.querySelector(id).scrollIntoView({
+				behavior: 'smooth',
+				block: 'start'
+			});
+		});
+	}
+}
+
 
 mobileBtn();
 langMenu();
 accordion();
 slider();
+smoothScroll();
